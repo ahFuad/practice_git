7176cb09995fcca9e58c433434cba8fe4e03527d
FuadCharon <ahsanfuad@gmail.com>
2018-04-21 18:34:23 +0600

    ADD greetings

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.io.PrintWriter;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Greetings");
     }
 }
